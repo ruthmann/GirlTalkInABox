138c75d9348cd52decbff9ee90012dd83af77295
Alex Ruthmann <alex.ruthmann@gmail.com>
2013-05-29 20:34:38 -0400

    Updates

---
+++ bootstrap/js/cufonConfig.js
@@ -0,0 +1,20 @@
+    Cufon.replace('h1', { fontFamily: 'Gotham Book' });
+    
+    Cufon.replace('h2', {
+        hover:true, // allows for normal link behavior
+        fontFamily: 'Gotham Book' 
+    });
+    
+    Cufon.replace('h3', {
+        hover:true, // allows for normal link behavior
+        fontFamily: 'Gotham Medium'  /* Gotham Medium */
+    });
+    
+    Cufon.replace('blockquote, li, ul, a, h4', {
+        hover:true, // allows for normal link behavior
+        fontFamily: 'Gotham Light' 
+    });
+    
+    Cufon.replace('.news.landing .summaryList h4', { fontFamily: 'Gotham Medium' });
+    Cufon.replace('.portlet_header_title', { fontFamily: 'Gotham Book' });
+    Cufon.replace('.portlet_header_full', { fontFamily: 'Gotham Book' });
