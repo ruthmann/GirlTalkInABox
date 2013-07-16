5c83e0677955dd717e8e977971edc44b2e5cd390
Alex Ruthmann <alex.ruthmann@gmail.com>
2013-07-16 17:34:14 -0400

    update

--- js/favsongs.js
+++ js/favsongs.js
@@ -1,14 +1,14 @@
 var favSongs = [
     {
-        "artist": "Gas House Gang", 
-        "id": "TRKQJMI13D1C8D8FDB", 
-        "title": "Hello, My Baby", 
+        "artist": "Cake", 
+        "id": "TRBGQNI13FD3FE8DA8", 
+        "title": "Sad Songs and Waltzes", 
         "visits": 62
     }, 
     {
-        "artist": "Nina Paley, arr. Aaron Wolf", 
-        "id": "TRNAAXC13D1CAFF7F2", 
-        "title": "Copying is Not Theft", 
+        "artist": "Radiohead", 
+        "id": "TRTXSUS13FB217D705", 
+        "title": "Drum Stem - Nude", 
         "visits": 110
     }
 ]
